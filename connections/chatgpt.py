6c5d9fc2e8b6c71c4aa6117cc1ae9195f04525bc
Bilal <alronz.bilal@gmail.com>
2023-06-04 21:26:09 +0200

    Add example for chatgpt use

--- connections/chatgpt.py
+++ connections/chatgpt.py
@@ -1,19 +1,30 @@
 import os
 import openai
-from dotenv import load_dotenv
-
-load_dotenv()
+import json
 
 
-def get_chatgpt_text_response(prompt_text: str) -> str:
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-    response = openai.Completion.create(
-        engine="text-davinci-002", prompt=prompt_text, max_tokens=60
-    )
-    response_text = response.choices[0].text
-    return response_text
+class ChatApp:
+    def __init__(self, setup_file_path):
+        openai.organization = os.getenv('OPENAI_ORG')
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+        with open(setup_file_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        self.messages = data
+
+    # "gpt-3.5-turbo" or "gpt-4"
+    def chat(self, prompt, model="gpt-3.5-turbo", tries=1):
+        self.messages.append({"role": "user", "content": prompt})
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=self.messages,
+            n=tries  # how many chat completion choices
+        )
+        return response["choices"][0]["message"].content
 
 
 if __name__ == "__main__":
-    prompt_text = """Who is the president of Yemen?"""
-    print(get_chatgpt_text_response(prompt_text))
+    os.environ['OPENAI_ORG'] = ""
+    os.environ['OPENAI_API_KEY'] = ""
+    chatgpt_clien = ChatApp(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), 'chatgpt_setup_data', 'awesome_list_context.json'))
+    print(chatgpt_clien.chat("The idea is AutoGPT and the data: "))
