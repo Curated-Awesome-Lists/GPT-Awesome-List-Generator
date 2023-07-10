0d08ff874524f1f605b9742840ba1b271de24693
alialsaeedi19 <alialsaeedi19@gmail.com>
2023-07-10 17:49:52 +0200

    code clean up

--- streamlit.py
+++ streamlit.py
@@ -2,48 +2,85 @@ import openai
 import streamlit as st
 from streamlit_chat import message
 
-# Setting page title and header
+
+def initialize_session_state():
+    keys = [
+        "openai_api_key",
+        "generated",
+        "past",
+        "messages",
+        "model_name",
+        "cost",
+        "total_tokens",
+        "total_cost",
+    ]
+
+    default_values = [
+        "",
+        [],
+        [],
+        [{"role": "system", "content": "You are a helpful assistant."}],
+        [],
+        [],
+        [],
+        0.0,
+    ]
+
+    for key, default_value in zip(keys, default_values):
+        if key not in st.session_state:
+            st.session_state[key] = default_value
+
+
+def reset_session_state():
+    st.session_state["generated"] = []
+    st.session_state["past"] = []
+    st.session_state["messages"] = [
+        {"role": "system", "content": "You are a helpful assistant."}
+    ]
+    st.session_state["model_name"] = []
+    st.session_state["cost"] = []
+    st.session_state["total_cost"] = 0.0
+    st.session_state["total_tokens"] = []
+
+
+def generate_response(prompt, model):
+    st.session_state["messages"].append({"role": "user", "content": prompt})
+
+    completion = openai.ChatCompletion.create(
+        model=model, messages=st.session_state["messages"]
+    )
+    response = completion.choices[0].message.content
+    st.session_state["messages"].append({"role": "assistant", "content": response})
+
+    usage_info = {
+        "total_tokens": completion.usage.total_tokens,
+        "prompt_tokens": completion.usage.prompt_tokens,
+        "completion_tokens": completion.usage.completion_tokens,
+    }
+    return response, usage_info
+
+
 st.set_page_config(page_title="AVA", page_icon=":robot_face:")
 st.markdown(
     "<h1 style='text-align: center;'>Awesome List Generator</h1>",
     unsafe_allow_html=True,
 )
-# Sidebar - ask for API key if not provided
-if "openai_api_key" not in st.session_state:
-    st.session_state["openai_api_key"] = ""
 
-# Map model names to OpenAI model IDs
+initialize_session_state()
+
 model_map = {"GPT-3.5": "gpt-3.5-turbo", "GPT-4": "gpt-4"}
 
 if not st.session_state["openai_api_key"]:
     with st.form(key="api_key_form"):
-        st.session_state["openai_api_key"] = st.text_input("Enter OpenAI API key:", type="password")
+        st.session_state["openai_api_key"] = st.text_input(
+            "Enter OpenAI API key:", type="password"
+        )
         api_key_submit = st.form_submit_button("Submit API Key")
 
     if api_key_submit:
         openai.api_key = st.session_state["openai_api_key"]
 else:
     openai.api_key = st.session_state["openai_api_key"]
-
-    # Initialise session state variables
-    if "generated" not in st.session_state:
-        st.session_state["generated"] = []
-    if "past" not in st.session_state:
-        st.session_state["past"] = []
-    if "messages" not in st.session_state:
-        st.session_state["messages"] = [
-            {"role": "system", "content": "You are a helpful assistant."}
-        ]
-    if "model_name" not in st.session_state:
-        st.session_state["model_name"] = []
-    if "cost" not in st.session_state:
-        st.session_state["cost"] = []
-    if "total_tokens" not in st.session_state:
-        st.session_state["total_tokens"] = []
-    if "total_cost" not in st.session_state:
-        st.session_state["total_cost"] = 0.0
-
-    # Sidebar - let user choose model, show total cost of current conversation, and let user clear the current conversation
     st.sidebar.title("Sidebar")
     model_name = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
     counter_placeholder = st.sidebar.empty()
@@ -52,45 +89,15 @@ else:
     )
     clear_button = st.sidebar.button("Clear Conversation", key="clear")
 
-    # Map model names to OpenAI model IDs
     model = model_map[model_name]
 
-    # Reset everything
     if clear_button:
-        st.session_state["generated"] = []
-        st.session_state["past"] = []
-        st.session_state["messages"] = [
-            {"role": "system", "content": "You are a helpful assistant."}
-        ]
-        st.session_state["number_tokens"] = []
-        st.session_state["model_name"] = []
-        st.session_state["cost"] = []
-        st.session_state["total_cost"] = 0.0
-        st.session_state["total_tokens"] = []
+        reset_session_state()
         counter_placeholder.write(
             f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}"
         )
 
-    # generate a response
-    def generate_response(prompt):
-        st.session_state["messages"].append({"role": "user", "content": prompt})
-
-        completion = openai.ChatCompletion.create(
-            model=model, messages=st.session_state["messages"]
-        )
-        response = completion.choices[0].message.content
-        st.session_state["messages"].append({"role": "assistant", "content": response})
-
-        # print(st.session_state['messages'])
-        total_tokens = completion.usage.total_tokens
-        prompt_tokens = completion.usage.prompt_tokens
-        completion_tokens = completion.usage.completion_tokens
-        return response, total_tokens, prompt_tokens, completion_tokens
-
-
-    # container for chat history
     response_container = st.container()
-    # container for text box
     container = st.container()
 
     with container:
@@ -99,19 +106,19 @@ else:
             submit_button = st.form_submit_button(label="Send")
 
         if submit_button and user_input:
-            output, total_tokens, prompt_tokens, completion_tokens = generate_response(
-                user_input
-            )
+            output, usage_info = generate_response(user_input, model)
             st.session_state["past"].append(user_input)
             st.session_state["generated"].append(output)
             st.session_state["model_name"].append(model_name)
-            st.session_state["total_tokens"].append(total_tokens)
+            st.session_state["total_tokens"].append(usage_info["total_tokens"])
 
-            # from https://openai.com/pricing#language-models
             if model_name == "GPT-3.5":
-                cost = total_tokens * 0.002 / 1000
+                cost = usage_info["total_tokens"] * 0.002 / 1000
             else:
-                cost = (prompt_tokens * 0.03 + completion_tokens * 0.06) / 1000
+                cost = (
+                    usage_info["prompt_tokens"] * 0.03
+                    + usage_info["completion_tokens"] * 0.06
+                ) / 1000
 
             st.session_state["cost"].append(cost)
             st.session_state["total_cost"] += cost
