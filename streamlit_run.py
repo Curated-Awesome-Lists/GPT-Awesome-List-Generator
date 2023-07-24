a31740ae2dab8a38e0b751a41652b88e6e470603
Bilal <alronz.bilal@gmail.com>
2023-07-24 23:01:44 +0200

    Simplify streamlit UI

--- streamlit_run.py
+++ streamlit_run.py
@@ -1,27 +1,11 @@
-from typing import Dict, Tuple
-
 import streamlit as st
-from streamlit_chat import message
-
 from awesome_list_generator import AwesomeListGenerator
 from utils import timing
 
 
 class AppState:
-    keys = [
-        "output",
-        "user_input",
-        "model_name",
-        "tokens",
-        "input_submitted",
-    ]
-    default_values = [
-        "",
-        "",
-        "",
-        0.0,
-        False,
-    ]
+    keys = ["output", "user_input", "model_name", "tokens", "input_submitted"]
+    default_values = ["", "", "", 0.0, False]
 
     @classmethod
     def initialize(cls) -> None:
@@ -36,69 +20,48 @@ class AppState:
 
 
 @timing
-def generate_response(
-    keyword: str, description: str, model: str
-) -> Tuple[str, Dict[str, int]]:
+def generate_response(keyword: str, description: str, model: str) -> tuple[str, dict[str, float]]:
     awesome_list_generator = AwesomeListGenerator(keyword, description, model, 10, 40)
     response, usage_info = awesome_list_generator.save_and_return_awesome_list()
     return response, usage_info
 
 
 def setup_streamlit():
-    # Streamlit configuration
     st.set_page_config(page_title="Awesome List Generator", page_icon=":robot_face:")
-
-    # Markdown for the application
-    st.markdown(
-        "<h1 style='text-align: center;'>Awesome List Generator</h1>",
-        unsafe_allow_html=True,
-    )
+    st.markdown("<h1 style='text-align: center; font-size:3em;'>Awesome List Generator</h1>", unsafe_allow_html=True)
 
 
 def setup_sidebar(model_map):
     st.sidebar.title("Sidebar")
     model_name = st.sidebar.radio("Choose a model:", tuple(model_map.keys()))
     counter_placeholder = st.sidebar.empty()
-
     return model_name, model_map[model_name]
 
 
 def setup_main_container():
-    if not st.session_state["input_submitted"]:
-        keyword = st.text_input("keyword:")
-        description = st.text_input("Describe the keyword in 1-2 sentences:")
-        submit_button_slot = st.empty()
-        submit_button = submit_button_slot.button(label="Create Awesome List")
+    st.markdown("""
+        This tool generates an awesome list based on your input parameters. It includes resources like GitHub projects, 
+        Google Scholar articles, YouTube videos, and podcasts. The awesome list is automatically generated using GPT models.
+    """)
 
-        if submit_button and keyword and description:
-            submit_button_slot.empty()
+    keyword = st.text_input("Keyword",
+                            help="The keyword is critical for good results because we use it to search for repos in GitHub or videos on YouTube, etc.")
+    description = st.text_area("Description",
+                               help="This description is used to filter the results with the LLM to ensure we have relevant results. It is also the description we show in the final markdown.")
+
+    if keyword and description:
+        create_button = st.empty()
+        if create_button.button("Create Awesome List"):
+            create_button.empty()
             with st.spinner(
-                "The awesome list is being generated, this could take some minutes "
-                "to finish. Please be patient"
-            ):
-                awesome_list, usage_info = generate_response(
-                    keyword, description, model
-                )
-            st.session_state.update(
-                {
-                    "input_submitted": True,
-                    "user_input": f"generate an awesome list for {keyword}",
-                    "output": awesome_list,
-                    "model_name": model_name,
-                    "tokens": usage_info["total_tokens"],
-                }
-            )
-            st.experimental_rerun()
-
-    else:
-        with st.container():
-            key = "0"
-            message(st.session_state["user_input"], is_user=True, key=key + "_user")
-            message(st.session_state["output"], key=key)
-            st.write(
-                f"Model used: {st.session_state['model_name']}; "
-                f"Number of tokens used: {st.session_state['tokens']}; "
-            )
+                    "The awesome list is being generated. This could take some minutes to finish. Please be patient."):
+                awesome_list, usage_info = generate_response(keyword, description, model)
+            st.markdown("Your Awesome List is Ready!")
+            st.markdown("---")
+            st.markdown(awesome_list, unsafe_allow_html=True)
+            st.markdown(f"Model used: {model_name}; Number of tokens used: {usage_info['total_tokens']};")
+            st.download_button("Download Awesome List", data=awesome_list, file_name=f"{model_name}_Awesome_List.md",
+                               mime="text/markdown")
 
 
 if __name__ == "__main__":
